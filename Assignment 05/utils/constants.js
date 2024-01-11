6f9d8f10028c40b5be5bc5380526cc4ff8c141a6
subuxd <103200387+subuxd@users.noreply.github.com>
2024-01-11 19:58:18 +0530

    Create constants.js

---
+++ Assignment 05/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://img.freepik.com/premium-vector/indian-restaurant-icon-chef-turban-spice-leaves_8071-11542.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1704153600&semt=ais";
