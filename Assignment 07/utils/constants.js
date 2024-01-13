7961b2f55b604012be19ecdf6b7dc2610a1ca1aa
subuxd <103200387+subuxd@users.noreply.github.com>
2024-01-14 02:53:00 +0530

    Create constants.js

---
+++ Assignment 07/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://img.freepik.com/premium-vector/indian-restaurant-icon-chef-turban-spice-leaves_8071-11542.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1704153600&semt=ais";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=20.2960587&lng=85.8245398&restaurantId=";
