55a60a69dd4f0239c37f71ef809fa0baa02cef0b
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-19 13:16:15 +0530

    Create App.js

---
+++ Assignment 08/src/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
+
+// if we dont have id, we can use indexes as id but it is not recommended written in the reactjs docs ex:
+// <div className="res-container">
+//         {restaurantList.map((restaurant, index) => (
+//           <RestaurantCard key={index} resData={restaurant} />
+//         ))}
+//       </div>
+
+// index increment by 1 at every step
