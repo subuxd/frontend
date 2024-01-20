64b2d04cd38ed9ac2a618ffbc8aa6f0ebf069545
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-21 01:07:54 +0530

    Create useOnlineStatus.js

---
+++ Assignment 09/src/utils/useOnlineStatus.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useOnlineStatus = () => {
+  const [OnlineStatus, setOnlineStatus] = useState(true);
+
+  //check if online
+  useEffect(() => {
+    window.addEventListener("offline", () => {
+      setOnlineStatus(false);
+    });
+
+    window.addEventListener("online", () => {
+      setOnlineStatus(true);
+    });
+  }, []);
+
+  //boolean value
+  return OnlineStatus;
+};
+
+export default useOnlineStatus;
