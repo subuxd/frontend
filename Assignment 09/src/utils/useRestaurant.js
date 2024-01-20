78d30cb9af5ff09e1c979cfe97886e8590afbb22
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-21 01:08:20 +0530

    Create useRestaurant.js

---
+++ Assignment 09/src/utils/useRestaurant.js
@@ -0,0 +1,73 @@
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+// chunking / code splitting / dynamic bundling / lazy loading / on demand loading - this means we'll load the bundled code once it is clicked by the user
+const Grocery = lazy(() => import("./components/Grocery"));
+
+const About = lazy(() => import("./components/About"));
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
+
+// if we dont have id, we can use indexes as id but it is not recommended written in the reactjs docs ex:
+// <div className="res-container">
+//         {restaurantList.map((restaurant, index) => (
+//           <RestaurantCard key={index} resData={restaurant} />
+//         ))}
+//       </div>
+
+// index increment by 1 at every step
