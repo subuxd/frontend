c348de3379f8604f0b367d34b18ee8b0fd11bc2e
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-22 01:50:30 +0530

    Create RestaurantCard.js

---
+++ Assignment 10/src/components/RestaurantCard.js
@@ -0,0 +1,33 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  cuisines,
+  areaName,
+  locality,
+  sla,
+  costForTwo,
+  avgRating,
+}) => {
+  return (
+    // here in inline css we used 2 sets of {{}} b/c the 1st is for {js} and the 2nd is for {js object}
+    <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-300">
+      <img
+        className="rounded-lg"
+        alt="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>
+        {locality},{areaName}
+      </h4>
+      <h4>{avgRating}⭐</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.slaString}🕥</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
