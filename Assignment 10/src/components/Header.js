c643412d8e04f3119e705264884f1bc353db35f0
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-22 01:49:30 +0530

    Create Header.js

---
+++ Assignment 10/src/components/Header.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  const onlineStatus = useOnlineStatus();
+
+  return (
+    // we can also use tailwind css to write media queries i.e. making our app responsive
+    <div className="flex justify-between bg-green-200 shadow-lg sm:bg-sky-300 lg:bg-orange-300">
+      <div className="logo-container">
+        <img className="w-32" src={LOGO_URL} alt="" />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/about">About us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/contact">Contact us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li className="px-4">Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
