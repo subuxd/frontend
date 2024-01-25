c09db3047ba41715e97de80d88fec1a4df52bb4d
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-26 02:39:35 +0530

    Create Body.js

---
+++ Assignment 11/src/components/Body.js
@@ -0,0 +1,116 @@
+import RestaurantCard, { withOpenLabel } from "./RestaurantCard";
+import { useContext, useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
+
+const Body = () => {
+  // local state variable is superpowerful
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const RestaurantCardOpen = withOpenLabel(RestaurantCard);
+
+  // whenever state variable update, react triggers a reconcilation cycle (re-renders the component)
+  console.log("Body Rendered", listOfRestaurants);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.2960587&lng=85.8245398&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    //optional chaining - ?. ?.
+    setlistOfRestaurants(
+      json?.data.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  const OnlineStatus = useOnlineStatus();
+
+  if (OnlineStatus === false)
+    return <h1>You're OFFLINE check your internet connection</h1>;
+
+  const { loggedInUser, setUserName } = useContext(UserContext);
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter flex">
+        <div className="search m-4 p-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            placeholder="Search a restaurant..."
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="px-4 py-1 bg-green-100 m-4 rounded-lg"
+            onClick={() => {
+              //filter the restaurant cards and update the UI
+              //searchText
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="m-4 p-4 flex items-center">
+          <label>UserName : </label>
+          <input
+            className="border border-black p-2"
+            value={loggedInUser}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.2
+              );
+              setFilteredRestaurant(filteredList);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-wrap">
+        {filteredRestaurant.map((restaurant) => (
+          <Link
+            key={restaurant.info.id}
+            to={"/restaurants/" + restaurant.info.id}
+          >
+            {/**if the restaurant is open add open label to it */}
+            {restaurant.info.isOpen ? (
+              <RestaurantCardOpen {...restaurant.info} />
+            ) : (
+              <RestaurantCard {...restaurant.info} />
+            )}
+          </Link>
+          // whenever using map dont forget to give key it helps to avoid re-rendering of all data whenever any new data is added i.e, only the new added data will be rendered
+          // The spread operator ({...restaurant.data}) is used to pass all properties of the restaurant.data object as props to the RestaurantCard component.
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
