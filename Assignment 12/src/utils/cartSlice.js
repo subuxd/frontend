ade7b316a934f2dd97099a37faaf115416faa2b8
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-28 01:54:31 +0530

    Create cartSlice.js

---
+++ Assignment 12/src/utils/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      // modifying the state here
+      state.items.push(action.payload);
+    },
+    removeItem: (state) => {
+      state.items.pop();
+    },
+    clearCart: (state) => {
+      state.items.length = 0;
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+// addItem is dispatch action and the (state, action) function is reducer function
