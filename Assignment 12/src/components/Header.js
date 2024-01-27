e8d672c09aefd65c631f54e3025e703db1330ced
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-28 01:53:25 +0530

    Create Header.js

---
+++ Assignment 12/src/components/Header.js
@@ -0,0 +1,60 @@
+import { useContext, useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
+
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  const onlineStatus = useOnlineStatus();
+
+  const { loggedInUser } = useContext(UserContext);
+  // console.log(data);
+
+  // Subscribing to the store using Selector
+  const cartItems = useSelector((store) => store.cart.items);
+
+  return (
+    // we can also use tailwind css to write media queries i.e. making our app responsive
+    <div className="flex justify-between bg-green-200 shadow-lg sm:bg-sky-300 lg:bg-orange-300">
+      <div className="logo-container">
+        <img className="w-32" src={LOGO_URL} alt="" />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/about">About us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/contact">Contact us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li className="px-4 font-bold">
+            <Link to="/cart">Cart ({cartItems.length} items)</Link>
+          </li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+          <li className="px-4 font-bold">{loggedInUser}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
