4d7009593d9c4f5513ede468b2dde35b196b8731
Subham <103200387+subuxd@users.noreply.github.com>
2024-01-30 02:54:20 +0530

    Create RestaurantCard.test.js

---
+++ Assignment 13/src/components/__tests__/RestaurantCard.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import RestaurantCard from "../RestaurantCard";
+
+it("Should render RestaurantCard component with props data", () => {
+  render(<RestaurantCard />);
+});
+
+//Here we will pass data and run the test case, will do in future
